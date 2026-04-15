5a660a580d0af6adc79496dc59c021df90f4d274
Satyam Lal <ssatyamlal@gmail.com>
2026-04-16 00:22:44 +0530

    read terminal commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args[1] == "init" {
+        fs::create_dir(".git").unwrap();
+        fs::create_dir(".git/objects").unwrap();
+        fs::create_dir(".git/refs").unwrap();
+        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+        println!("Initialized git directory!");
+    } else {
+        println!("Unknown command: {}", args[1]);
+    }
 }
